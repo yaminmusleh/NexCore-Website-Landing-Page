6c10b15d02e2859800a9f65a07cd7f9e1a91fcf1
yaminmusleh <yaminaktham3@gmail.com>
2026-07-17 16:43:16 +0300

    fixed a problem

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Arsenal from '../components/About/Arsenal'
+
 import StorySection from '../components/about/StorySection'
 import AboutMe from '../components/about/AboutMe'
+import Arsenal from '../components/about/Arsenal'
 
 export default function About() {
   return (
     <>
     <StorySection/>
     <AboutMe/>
-    <Arsenal />
+    <Arsenal/>
     
     </>
   )
