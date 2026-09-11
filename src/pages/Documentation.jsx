a7714395adf03f85a0d196a94dec95b0b1e055ea
yaminmusleh <yaminaktham3@gmail.com>
2026-09-11 19:45:43 +0300

    lazy to continue

--- src/pages/Documentation.jsx
+++ src/pages/Documentation.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import Box from "@mui/material/Box";
+import { Grid } from "lucide-react";
 
 export default function Documentation() {
   return (
-    <div>Documentation</div>
-  )
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          backgroundColor: "#1C0D24",
+          px: { xs: 3, sm: 5, md: 8 },
+          py: { xs: 6, md: 8 },
+        }}
+      >
+        <Grid spacing={3}>
+
+        </Grid>
+      </Box>
+    </>
+  );
 }
